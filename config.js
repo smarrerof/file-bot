4923ed08621ce7d378ebc7f28ce71c0772f4ca6d
Sergio Marrero <smarrerof@gmail.com>
2025-03-12 17:05:02 +0000

    Set default path for downloads in configuration

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ dotenv.config();
 const config = {
   token: process.env.TELEGRAM_BOT_TOKEN,
   id: Number(process.env.TELEGRAM_CHAT_ID),
-  defaultPath: process.env.DOWNLOAD_DEFAULT_PATH,
+  defaultPath: process.env.DOWNLOAD_DEFAULT_PATH ?? '/downloads',
 };
 
 export default config;
